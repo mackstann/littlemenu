0ceda46e42223b17b77f8fcece21e208ed166699
Nick Welch <mack@incise.org>
2008-10-07 18:52:24 -0700

    updating copyright notices

--- littlemenu.py
+++ littlemenu.py
@@ -22,6 +22,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+# this script is still very ugly
+
 import os, random, time, sys
 import pygame
 from pygame import font, joystick, event
